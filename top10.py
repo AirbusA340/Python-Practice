07c234027aff3c2eeaf24be4b8d21fc60e94cab9
AirbusA340 <elims@wsc.school.nz>
2024-05-30 12:26:07 +1200

    finish

--- top10.py
+++ top10.py
@@ -1,25 +1,23 @@
-import random
-
-score = 0
-MAX_TURNS = 10
-ANSWERS = ["nobody, nothing, north, nickles, never, noggin, nicely, nice, niece, niger"]
+guesses = []
+ANSWERS = ["b747", "b757" ,"b767" ,"b777" ,"b787" ,"a320" ,"a330" ,"a340" ,"a350", "a380"]
 
 # FUNCTIONS
 
+# Welcomes User
 def intro():
+    # Asks for the name
     name = input("What is your username?")
 
-    print("Welcome to the top 10 quiz.", name)
-    print("This quiz is about the top 10 n-words.")
+    #Greets the user
+    print("Hello {}, welcome to the top 10 quiz.".format(name))
+    print("This quiz is about the top 10 planes.")
 
-# MAIN CODE
-
-intro()
-
+# Asks user for lives
 def getlives():
     while True:
         lives = input("How many chances do you want?")
         try:
+            #validation check
             lives = int(lives)
             if lives >= 0:
                 return lives
@@ -28,21 +26,36 @@ def getlives():
         except:
             print("That wasn't a number")
 
-lives = getlives()
-
-def isCorrect(answer, ANSWERS):
-    if answer in ANSWERS:
+# Checks if answer already exists in the list. Used for both correct answers and previous guesses
+def inList(answer, list):
+    if answer in list:
         return True
     else:
         return False
-score = 0
-while lives > 0:
-    answer = input("Name the top 10 n-words.")
 
-    if isCorrect(answer, ANSWERS) == True:
-        print("Correct")
-        score += 1
+# MAIN CODE
+
+intro()
+lives = getlives()
+score = 0
+
+#Begin quiz
+while lives > 0:
+    answer = input("Name the top 10 planes.")
+    # Checks if correct or wrong
+    if inList(answer, ANSWERS):
+        # Checks if already guessed or not
+        if inList(answer, guesses):
+            print("You've guessed that already")
+        else:
+            print("Correct")
+            score += 1
+            guesses.append(answer)
+            print("You have guessed {}. Your score is {}. You have {} remaining".format(len(guesses), score))
     else:
         print("Wrong")
         lives -= 1
-                          
+        print("You have guessed {}. Your score is {}. You have {} remaining".format(len(guesses), score))
+
+# End game
+print("Game Over, your final score is {}".format(score))              
